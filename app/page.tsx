06ad0cefa6dce0f184ceaea657242567217947ce
MANFLU <kim@orsoe.me>
2024-02-05 08:41:23 +0100

    init

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,13 @@
+import { Container, Grid, Skeleton } from "@mantine/core";
+
 export default function HomePage() {
-  return <div>Home page</div>;
+  return (
+    <Container>
+      <Grid>
+        <Grid.Col span={4}>
+          <Skeleton height="30em" radius="md" animate={false} />
+        </Grid.Col>
+      </Grid>
+    </Container>
+  );
 }
